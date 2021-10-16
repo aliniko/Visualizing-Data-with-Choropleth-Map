65a5392af61ea391bb9fe65532d7dbefe9d4a300
aliniko <aliverchan@gmail.com>
2021-10-16 12:23:40 +0200

    "data-fips" and "data-education"

--- script.js
+++ script.js
@@ -5,59 +5,69 @@
 // - canvas is a d3 selection of the svg canvas to reference easily
 // - drawMap() is a function that will draw the map, once we have obtained the data
 
-let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
-let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+let countyURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+let educationURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
 
-let countyData
-let educationData
+let countyData;
+let educationData;
 
-let canvas = d3.select('#canvas')
+let canvas = d3.select("#canvas");
 
 let drawMap = () => {
- canvas.selectAll('path')
-        .data(countyData)
-        .enter()
-        .append('path')
-        .attr('d', d3.geoPath())
-        .attr('class', 'county')
-        .attr('fill', (countyDataItem) => {
-            let id = countyDataItem['id']
-            let county = educationData.find((item) => {
-                return item['fips'] === id
-            })
-            let percentage = county['bachelorsOrHigher']
-            if(percentage <= 15){
-                return 'tomato'
-            }else if(percentage <= 30){
-                return 'orange'
-            }else if(percentage <= 45){
-                return 'lightgreen'
-            }else{
-                return 'limegreen'
-            }
-        })
-}
+  canvas
+    .selectAll("path")
+    .data(countyData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county")
+    .attr("fill", (countyDataItem) => {
+      let id = countyDataItem["id"];
+      let county = educationData.find((item) => {
+        return item["fips"] === id;
+      });
+      let percentage = county["bachelorsOrHigher"];
+      if (percentage <= 15) {
+        return "tomato";
+      } else if (percentage <= 30) {
+        return "orange";
+      } else if (percentage <= 45) {
+        return "lightgreen";
+      } else {
+        return "limegreen";
+      }
+    })
+    // 5. My counties should each have "data-fips" and "data-education" properties containing their corresponding fips and education values
+    .attr("data-fips", (countyDataItem) => {
+      return countyDataItem["id"];
+    })
+    .attr('data-fips', (countyDataItem) => {
+        let id = countyDataItem["id"];
+        let county = educationData.find((item) => {
+          return item["fips"] === id;
+        });
+        let percentage = county["bachelorsOrHigher"];
+    });
+};
 
-d3.json(countyURL).then(
-    (data, error) => {
-        if(error){
-            console.log(log)
-        }else {
-            countyData = topojson.feature(data, data.objects.counties).features
-            console.log("County Data")
-            console.log(countyData)
-        }
-        d3.json(educationURL).then(
-            (data, error) => {
-                if(error){
-                    console.log(log)
-                }else {
-                    educationData = data
-                    console.log("Education Data")
-                    console.log(educationData)
-                    drawMap()
-                }
-            }
-                    )
+d3.json(countyURL).then((data, error) => {
+  if (error) {
+    console.log(log);
+  } else {
+    countyData = topojson.feature(data, data.objects.counties).features;
+    console.log("County Data");
+    console.log(countyData);
+  }
+  d3.json(educationURL).then((data, error) => {
+    if (error) {
+      console.log(log);
+    } else {
+      educationData = data;
+      console.log("Education Data");
+      console.log(educationData);
+      drawMap();
     }
-)
+  });
+});
